94b8cf8cf6e0f087db740daa72ba65f57ce1c494
paoka <paoka@LAPTOP-1016MKVC>
2022-03-02 16:52:17 +0200

    Add project files.

---
+++ coding-school-session-2b/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//1
+
+Console.WriteLine("Hello Achilleas Blekos");
+
+
+//2
+
+int i, j, k;
+k = i + j;
+decimal d = i / j;
+Console.WriteLine(k);
+Console.WriteLine(d);
+
+//4
+
+int age;
+string gender;
+age = Console.ReadLine();
+gender = Console.ReadLine();
+Console.Write("You are {0} and look younger than {0} ",gender, age);
+
+//7
+
+Console.Write("Enter the amount of Celsius: ");
+int celsius = (Console.ReadLine());
+
+Console.WriteLine("Kelvin = {0}", celsius + 273);
+Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+
+
+
+
+
+
+
